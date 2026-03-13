cbb156c67bb7cb771e40fa6d12b655e8a4fa3fcf
falmiabdi <falmitesfaye@gmail.com>
2026-03-14 02:15:07 +0300

    this is update home page

---
+++ components/cartButton.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+
+const CartButton = () => {
+    const totalItems = 10;
+    return (
+        <TouchableOpacity>
+            <Text>CarButton</Text>
+        </TouchableOpacity>
+    );
+};
+
+export default CartButton;
